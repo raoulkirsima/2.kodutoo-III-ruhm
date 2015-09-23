53d2e720b731b1b0044914cc232d02300ad4d509
Raoul Kirsima <raoukir@greeny.cs.tlu.ee>
2015-09-23 13:38:08 +0300

    modified create user details

--- login.php
+++ login.php
@@ -17,79 +17,82 @@
 	$email_create ="";
 	$password_create= "";
 	
+	//*********************************************************************************
 	
 	//kontrollime, et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST" ) {
 		
-		//echo "keegi vajutas nuppu";
-		//kontrollin, et email ei ole tühi
 		if(isset($_POST["login"])){
-			echo "vajutas login nuppu!";
-		
-		
-		if ( empty($_POST["email_login"]) ) {
-			$email_error_login = "See väli on kohustuslik";
-		}else{
-			$email_login = test_input($_POST["email_create"]);
-		}
-		//kontrollin, et parool ei ole tühi
-		
-		if (empty($_POST["password_login"]) ) {
-			$password_error_login = "See väli on kohustuslik";
-		}else{
-			$password_login = test_input($POST["email"]);
-		} 
-		
-		if($email_error == "" && $password_error == ""){
-			echo "Ready to log in! Username is ".$email_login."and password is".$password_login;
 			
-			$hash = hash("sha512", $password_login);
-			
-			$stmt = mysqli ->prepare("SELECT id, email FROM user_sample WHERE email=?")
-			$stmt->bind_param("ss", $email, $hash);
-			
-			//muutujad tulemustele
-			$stmt->bind_result($id_from_db, $email_from_db);
-			$stmt->execute();
-			
-			//Kontrollin kas tulemusi leiti
-			if($stmt->fetch()){
-				//ab'is oli midagi
-				echo " E-mail and password are correct, user id=".$id_from_db;
+			if ( empty($_POST["email_login"]) ) {
+				$email_error_login = "This field is obligatory, you cannot leave it empty";
 			}else{
-				//ei olnud midagi
-				echo " Wrong credentials!";
+				$email_login = test_input($_POST["email_create"]);
 			}
+			//kontrollin, et parool ei ole tühi
+		
+			if (empty($_POST["password_login"]) ) {
+				$password_error_login = "This field is obligatory, you cannot leave it empty";
+			}else{
+				$password_login = test_input($POST["password_login"]);
+			} 
+		
+			if($email_error == "" && $password_error == ""){
+				echo "Ready to log in! Username is ".$email_login."and password is".$password_login;
 			
-			$stmt->close();
-		}	
+				$hash = hash("sha512", $password_login);
 			
-	//***************************************************************************		
-	    }elseif(isset($_POST["create"])){	
-			echo" vajutas create user nuppu!";	
-	
-	
-			if (empty($_POST["e-mail"])){
-			$email_error2 = "See väli on kohustuslik";
-			}
-			if (empty($_POST["pass"]) ) {
-			$password_error2 = "See väli on kohustuslik";
-			} else {
+				$stmt = mysqli ->prepare("SELECT id, email FROM user_sample WHERE email=?")
+				$stmt->bind_param("ss", $email, $hash);
 			
-				if(strlen($_POST["pass"]) <8) {
-				$password_error2 = "Parool on liiga lühike, peab olema vähemalt 8 tähemärki pikk!";
+				//muutujad tulemustele
+				$stmt->bind_result($id_from_db, $email_from_db);
+				$stmt->execute();
+			
+				//Kontrollin kas tulemus leiti
+				if($stmt->fetch()){
+					//ab'is oli midagi
+					echo " E-mail and password are correct, user id=".$id_from_db;
+				}else{
+					//ei olnud midagi
+					echo " Wrong credentials!";
 				}
-				$sname = test_input($_POST["name"]);
-	
-			}
-			if (empty ($_POST["first_name"])){
-			$first_name_error = "See väli on kohustuslik";
+			
+				$stmt->close();
+			}	
+		}
+		
+	 //***************************************************************************		
+	    if(isset($_POST["create"])){		
+			if (empty($_POST["e-mail_create"])){
+				$email_error_create = "This field is obligatory, you cannot leave it empty";
+			}else{
+				$email_create = test_input($_POST["e-mail_create"]);
+			}	
+			
+			if (empty($_POST["password_create"])){
+				$password_error_create = "This field is obligatory, you cannot leave it empty";
+			}else{
+				if(strlen($_POST["password_create"]) <8) {
+				$password_error_create = "Password is too short, it has to be at least 8 characters long!";
+				}else{
+				$password_create = test_input($_POST["password_create"]);
+				}
 			}
 			
+			if (empty ($_POST["first_name"])){
+				$first_name_error = "This field is obligatory, you cannot leave it empty";
+			}else{
+				$first_name = test_input($_POST["first_name"]);
 			}
+			
 			if (empty ($_POST["last_name"])){
-			$last_name_error = "See väli on kohustuslik";
+				$last_name_error = "This field is obligatory, you cannot leave it empty";
+			}else{
+				$last_name = test_input($_POST["last_name"]);
 			}
+			
+			
 	}	
 	
 	
@@ -102,7 +105,6 @@
 	
 	
 	
-	//TÄIENDA INPUTE - NÄIDISE JÄRGI
 	
 	
 	
@@ -124,8 +126,8 @@
 	<h2>Log in</h2>
 		
 		<form action="login.php" method="post" >
-			<input name="email" type="email" placeholder="e-mail"> <?php echo $email_error_login; ?> <br><br>
-			<input name="password" type="password" placeholder="password"> <?php echo $password_error_login; ?> <br><br>
+			<input name="email_login" type="email" placeholder="e-mail"> <?php echo $email_error_login; ?> <br><br>
+			<input name="password_login" type="password" placeholder="password"> <?php echo $password_error_login; ?> <br><br>
 			<input name="login" type="submit" value="Log in" >
 		</form>
 		
@@ -133,8 +135,8 @@
 	<h2>Create user</h2>
 		
 		<form action="login.php" method="post" >
-			<input name="e-mail" type="email" placeholder="e-mail" >*<?php echo $email_error_create; ?> <br><br>
-			<input name="pass" type="password" placeholder="password" >*<?php echo $password_error_create; ?> <br><br><br><br>
+			<input name="e-mail_create" type="email" placeholder="e-mail" >*<?php echo $email_error_create; ?> <br><br>
+			<input name="password_create" type="password" placeholder="password" >*<?php echo $password_error_create; ?> <br><br><br><br>
 			<input name="first_name" type="text" placeholder="first name" >* <?php echo $first_name_error; ?> <br><br>
 			<input name="last_name" type="text" placeholder="last name" >* <?php echo $last_name_error; ?> <br><br>
 			<input name="create" type="submit" value="Create user" >
